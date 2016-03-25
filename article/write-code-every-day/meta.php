c6364409b75e40de6d01ff373f373b74f53e988e
Pavel Volyntsev <pvolyntsev@users.noreply.github.com>
2016-03-25 11:35:23 +0600

    Create meta.php

---
+++ article/write-code-every-day/meta.php
@@ -0,0 +1,20 @@
+<?php
+return array (
+	'info' => array (
+		'page-mode' => 'article',
+		'orig-language' => 'english',
+		'translate-language' => 'russian',
+
+		'orig-title' => 'Write Code Every Day',
+		'orig-url' => 'http://ejohn.org/blog/write-code-every-day/',
+		'orig-author-url' => 'http://ejohn.org/about/',
+		'orig-author-title' => 'John Resig',
+		'orig-tags' => 'code, coding, programming, study',
+
+		'trans-title' => 'Пишите код каждый день',
+		'trans-url' => 'http://habrahabr.ru/post/219209/',
+		'trans-author-url' => 'http://habrahabr.ru/users/ilya42/',
+		'trans-author-title' => 'Илья Сименко',
+		'trans-tags' => 'код, программный код, программирование, кодирование, изучение, обучение',
+	),
+);
