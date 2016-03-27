1c3f312d852878c9240616e63d705059b72da6b6
Pavel Volyntsev <pavel@visualpharm.com>
2016-03-28 03:01:35 +0600

    added article "Things Everyone Should Do: Code Review"

---
+++ article/things-everyone-should do-code-review/meta.php
@@ -0,0 +1,20 @@
+<?php
+return array (
+	'info' => array (
+		'page-mode' => 'article',
+		'orig-language' => 'english',
+		'translate-language' => 'russian',
+
+		'orig-title' => 'Things Everyone Should Do: Code Review',
+		'orig-url' => 'http://goodmath.scientopia.org/2011/07/06/things-everyone-should-do-code-review/',
+		'orig-author-url' => 'http://goodmath.scientopia.org/author/markcc/',
+		'orig-author-title' => 'MarkCC',
+		'orig-tags' => 'code review, engineering, programming',
+
+		'trans-title' => 'Вещи, которые должен делать каждый: Code Review',
+		'trans-url' => 'https://habrahabr.ru/post/215119/',
+		'trans-author-url' => 'https://habrahabr.ru/users/limonte/',
+		'trans-author-title' => '@limonte',
+		'trans-tags' => 'code review, teamwork, coworking',
+	),
+);
